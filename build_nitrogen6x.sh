f179a1156d6243114f1c13ecdb6f879b5448139b
Ivan Zaitsev <ivan.zaitsev@gmail.com>
2015-06-18 12:30:23 -0700

    Script to build kernel for nitrogen6x board.

---
+++ build_nitrogen6x.sh
@@ -0,0 +1,3 @@
+make nitrogen6x_defconfig ARCH=arm CROSS_COMPILE=../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+make $1 uImage ARCH=arm LOADADDR=0x10008000 CROSS_COMPILE=../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+make dtbs ARCH=arm CROSS_COMPILE=../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
