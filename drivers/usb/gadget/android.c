82085a0b7c1a8e531fef4ce5518c0500ba83721f
Ivan Zaitsev <ivan.zaitsev@gmail.com>
2015-07-24 17:50:40 -0700

    Remove 500ma Android gadget current claim.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -152,7 +152,6 @@ static struct usb_configuration android_config_driver = {
 	.unbind		= android_unbind_config,
 	.bConfigurationValue = 1,
 	.bmAttributes	= USB_CONFIG_ATT_ONE | USB_CONFIG_ATT_SELFPOWER,
-	.MaxPower	= 500, /* 500ma */
 };
 
 static void android_work(struct work_struct *data)
